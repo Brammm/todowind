5b9373b38e59bf08b3cdfa52dbe28193ead6d34b
Bram Van der Sype <bram.vandersype@gmail.com>
2019-11-06 21:55:09 +0100

    Installed Tailwindcss

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from "./components/Layout/Layout";
 
+import '../styles/todo.css';
+
 const Todo = () => {
     return <Layout
         header="This is my header">
