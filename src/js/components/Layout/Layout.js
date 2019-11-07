7106f5a7e0546118ba4eeeb2adab7d99ba405a6b
Bram Van der Sype <bram.vandersype@gmail.com>
2019-11-07 10:38:06 +0100

    Some minimalist styling for the layout

--- src/js/components/Layout/Layout.js
+++ src/js/components/Layout/Layout.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Layout = ({children, header}) => {
     return (
-        <>
-            <nav>
+        <div className="w-full flex">
+            <nav className="w-1/4 xl:w-1/5 bg-gray-800 text-gray-100">
                 Navigation
             </nav>
-            <header>
-                <h1>{header}</h1>
-            </header>
-            <main>
-                {children}
+            <main className="flex-1 text-gray-700">
+                <header className="bg-white">
+                    <h1>{header}</h1>
+                </header>
+                <div>
+                    {children}
+                </div>
             </main>
-        </>
+        </div>
     )
 };
 
