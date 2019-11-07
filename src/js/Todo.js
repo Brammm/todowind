32f5bb41c482f130a2392fb13dfe493873d0c08c
Bram Van der Sype <bram.vandersype@gmail.com>
2019-11-07 12:56:28 +0100

    Installed FontAwesome

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from "./components/Layout/Layout";
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/todo.css';
 
 const Todo = () => {
