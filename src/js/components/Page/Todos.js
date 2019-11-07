3bf4e5d8edf7c273a557cc81fa50df1904795d65
Bram Van der Sype <bram.vandersype@gmail.com>
2019-11-07 20:52:44 +0100

    Added html for todo page

--- src/js/components/Page/Todos.js
+++ src/js/components/Page/Todos.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import Layout from "../Layout/Layout";
+import Icon from "../Icon/Icon";
 
 const Todos = () => (
     <Layout>
-        Todos
+        <section>
+            <div>
+                <Icon name="tasks" />
+            </div>
+            <div>
+                <h2>Todos</h2>
+                <p>Below is a list of your todos. You can mark them as done, assign them to users or add new ones.</p>
+                <ul>
+                    <li>
+                        <h3><input type="checkbox" /> Run Create React App to bootstrap an app</h3>
+                        <small>Assigned to Bram</small>
+                    </li>
+                    <li>
+                        <h3><input type="checkbox" /> Get Tailwind playing nice with CRA</h3>
+                        <small>Assigned to Bram</small>
+                    </li>
+                    <li>
+                        <h3><input type="checkbox" /> Add some libraries and play around with them</h3>
+                        <small>Assigned to Bram</small>
+                    </li>
+                </ul>
+            </div>
+        </section>
     </Layout>
 );
 
