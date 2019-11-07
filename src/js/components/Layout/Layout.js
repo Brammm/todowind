cea67ef568ae71454c1443cdbd83cd0833e7ef20
Bram Van der Sype <bram.vandersype@gmail.com>
2019-11-07 14:10:06 +0100

    Clean up html

--- src/js/components/Layout/Layout.js
+++ src/js/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 const Layout = ({children, header}) => {
     const [showNavigation, toggleNavigation] = useState(false);
 
-    let navClassName = 'w-full fixed z-40 lg:w-1/4 xl:w-1/5 bg-gray-800 text-gray-100 lg:block h-screen';
+    let navClassName = 'w-full fixed z-50 lg:w-1/4 xl:w-1/5 bg-gray-800 text-gray-100 lg:block h-screen shadow-2xl';
     if (! showNavigation) {
         navClassName = navClassName + ' hidden';
     }
@@ -18,7 +18,7 @@ const Layout = ({children, header}) => {
 
     return (
         <>
-            <div className="fixed w-full z-50 flex flex-justify-between h-16 bg-gray-800 text-gray-100 items-center">
+            <header className="fixed w-full z-40 flex flex-justify-between h-16 bg-gray-800 text-gray-100 items-center shadow-l">
                 <h1 className="mr-auto w-1/4 xl:w-1/5 text-center text-teal-400 text-2xl font-bold h-8 tracking-widest uppercase">
                     Todowind
                 </h1>
@@ -30,14 +30,12 @@ const Layout = ({children, header}) => {
                 <button onClick={handleMenuToggle}>
                     <i className="fas fa-bars fa-border rounded mr-4 text-gray-500 border-gray-500 lg:hidden hover:text-white hover:border-white" />
                 </button>
-            </div>
+            </header>
             <div className="w-full pt-16 content-end lg:clearfix">
-                <aside className={navClassName}>
-                    <nav>
-                        <a className={navLinkClassName} href="/"><i className="fas fa-tasks mr-4"/>Todos</a>
-                        <a className={navLinkClassName} href="/"><i className="fas fa-users mr-4"/>Users</a>
-                    </nav>
-                </aside>
+                <nav className={navClassName}>
+                    <a className={navLinkClassName} href="/"><i className="fas fa-tasks mr-4"/>Todos</a>
+                    <a className={navLinkClassName} href="/"><i className="fas fa-users mr-4"/>Users</a>
+                </nav>
                 <main className="text-gray-700 lg:float-right lg:w-3/4 xl:w-4/5">
                     <header className="bg-white p-4 border-b sticky">
                         <h1 className="text-3xl text-gray-900">{header}</h1>
